550d7969b2b24587c7eb22c745d3b62d7c55e0d5
Desa Garut <desagarut.net@gmail.com>
2022-02-02 18:45:06 +0700

    Hapus Log error
    Hapus Log Error

--- logs/log-2022-01-29.php
+++
@@ -1,5 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2022-01-29 01:33:46 --> Unable to connect to the database
-ERROR - 2022-01-29 01:34:43 --> 404 Page Not Found: First/toko_warga
-ERROR - 2022-01-29 17:42:45 --> Severity: error --> Exception: Function name must be a string F:\laragon\www\sidega2022\donjo-app\views\toko_warga\table_produk.php 115
