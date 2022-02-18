153d8c07399cbf830ac0cf0bb51ffe0475e0756d
Desa Garut <desagarut.net@gmail.com>
2022-02-18 20:05:32 +0700

    gitnore
    gitnore

--- logs/log-2022-02-18.php
+++
@@ -1,5 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2022-02-18 12:57:02 --> Unable to connect to the database
-ERROR - 2022-02-18 12:57:02 --> Unable to connect to the database
-ERROR - 2022-02-18 12:57:02 --> Unable to connect to the database
