df547a50589f42d08fea7e52904d06e902eab3d2
Desa Garut <desagarut.net@gmail.com>
2022-02-18 23:35:02 +0700

    Lambaran Mimiti
    Lambaran Mimiti

--- donjo-app/controllers/Tawa.php
+++ donjo-app/controllers/Tawa.php
@@ -22,7 +22,7 @@ class Tawa extends Admin_Controller {
 	{
 		unset($_SESSION['cari']);
 		unset($_SESSION['filter']);
-		redirect('index');
+		redirect('tawa');
 	}
 
 	public function index($p=1, $o=0)
