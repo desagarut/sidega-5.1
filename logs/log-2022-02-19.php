cc1719bef0ea6d7d8c87dbf9094aa8377d68148b
Desa Garut <desagarut.net@gmail.com>
2022-02-19 15:25:23 +0700

    Update UMKM
    Update Peta Lokasi Wisata,

---
+++ logs/log-2022-02-19.php
@@ -0,0 +1,5 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2022-02-19 15:18:17 --> Severity: error --> Exception: syntax error, unexpected '<' F:\laragon\www\sidega-5.1\donjo-app\views\wisata\peta.php 25
+ERROR - 2022-02-19 15:18:57 --> Severity: error --> Exception: syntax error, unexpected '<', expecting end of file F:\laragon\www\sidega-5.1\donjo-app\views\wisata\peta.php 25
+ERROR - 2022-02-19 15:18:59 --> Severity: error --> Exception: syntax error, unexpected '<', expecting end of file F:\laragon\www\sidega-5.1\donjo-app\views\wisata\peta.php 25
